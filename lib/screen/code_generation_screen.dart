4ad92f33ff20ef83a6615c22a0ad84c7d623a6b0
Eric Lee <ericlee@Erics-MacBook-Air.local>
2024-01-31 19:21:33 +0900

    riverpod code generation added

---
+++ lib/screen/code_generation_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example/layout/default_layout.dart';
+import 'package:riverpod_example/riverpod/code_generation_provider.dart';
+
+class CodeGeneratioinScreen extends ConsumerWidget {
+  const CodeGeneratioinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+
+    final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
+    final state4 = ref.watch(gStateMultiplyProvider(
+      number1: 10,
+      number2: 20,
+    ));
+
+    return DefaultLayout(
+        title: 'CodeGenerationScreen',
+        body: Column(children: [
+          Text('State1: $state1'),
+          state2.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => Center(child: CircularProgressIndicator()),
+          ),
+          state3.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => Center(child: CircularProgressIndicator()),
+          ),
+          Text('State4: $state4'),
+          Consumer(builder: (context, ref, child) {
+            final state5 = ref.watch(gStateNotifierProvider);
+
+            return Text('State4: $state5');
+          }),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    ref.read(gStateNotifierProvider.notifier).increment();
+                  },
+                  child: Text('Increment')),
+              ElevatedButton(
+                  onPressed: () {
+                    ref.read(gStateNotifierProvider.notifier).decrement();
+                  },
+                  child: Text('Decrement')),
+            ],
+          ),
+          ElevatedButton(
+              onPressed: () {
+                ref.invalidate(gStateNotifierProvider);
+              },
+              child: Text('Invalidate'))
+        ]));
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_example/screen/auto_dispose_modifier_screen.dart';
+import 'package:riverpod_example/screen/code_generation_screen.dart';
 import 'package:riverpod_example/screen/family_modifier_screen.dart';
 import 'package:riverpod_example/screen/future_provider_screen.dart';
 import 'package:riverpod_example/screen/listen_provider_screen.dart';
@@ -101,6 +102,15 @@ class HomeScreen extends StatelessWidget {
                   );
                 },
                 child: Text('ProviderScreen')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => CodeGeneratioinScreen(),
+                    ),
+                  );
+                },
+                child: Text('CodeGeneratioinScreen')),
           ],
         ));
   }
